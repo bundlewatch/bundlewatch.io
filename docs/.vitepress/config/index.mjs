0d4d252cf1127d43a3002631bcce477d44ebc5b3
Nicolas Charpentier <nicolas.charpentier079@gmail.com>
2025-07-04 13:26:46 -0400

    Remove zh language for now

--- docs/.vitepress/config/index.mjs
+++ docs/.vitepress/config/index.mjs
@@ -1,20 +1,13 @@
 import { defineConfig } from "vitepress";
 import { shared } from "./shared.mjs";
-import { zh } from "./zh.mjs";
 import { en } from "./en.mjs";
 
-
-
 const config = {
-    ...shared,
+  ...shared,
 
-    locales: {
-        root: { label: "English", ...en },
-        zh: { label: "简体中文", ...zh },
-    },
-}
-
-
-
+  locales: {
+    root: { label: "English", ...en },
+  },
+};
 
 export default defineConfig(config);
